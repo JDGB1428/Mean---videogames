055b6fcbc9d95571ccb98962f406ff8c695704c6
JDGB1428 <juangalindo28@hotmail.com>
2023-11-21 11:38:21 -0500

    modificacion en el route

--- src/routes/games.route.js
+++ src/routes/games.route.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
+import {
+    getGames,
+    getGame,
+    createGames,
+    updateGames,
+    deleteGames
+} from "../controllers/games.controller.js";
+
 
 const router = Router();
 
-router.get('/games', authRequired, (req, res)=> res.send('games'))
+router.get('/games', authRequired, getGames);
+router.get('/games/:id', authRequired, getGame);
+router.post('/games', authRequired, createGames);
+router.delete('/games/:id', authRequired, deleteGames);
+router.put('/tasks/:id', authRequired, updateGames);
 
 export default router
