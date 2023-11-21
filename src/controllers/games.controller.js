a4397603c7e1da6d1ef4e01ae30624e2a766a933
JDGB1428 <juangalindo28@hotmail.com>
2023-11-21 11:37:11 -0500

    creacion del controlador games

---
+++ src/controllers/games.controller.js
@@ -0,0 +1,10 @@
+
+export const getGames = async(req,res) => {};
+
+export const createGames = async(req,res) => {};
+
+export const getGame = async(req,res) => {};
+
+export const updateGames = async(req,res) => {};
+
+export const deleteGames = async(req,res) => {};
