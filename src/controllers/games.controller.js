2bb926dfc0304856d3b167168733a9352c27330d
JDGB1428 <juangalindo28@hotmail.com>
2023-11-22 12:32:32 -0500

    Modificaciones del controller y modelo game, añadimos los modelos likes y comentarios

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,10 +1,49 @@
+import Games from "../models/games.model.js";
 
-export const getGames = async(req,res) => {};
 
-export const createGames = async(req,res) => {};
+export const getGames = async(req,res) => {
+    const games = await Games.find();
+    res.json(games);
+};
 
-export const getGame = async(req,res) => {};
+export const createGames = async(req,res) => {
+    const {title, description, price, platform, category, publication_date} = req.body;
 
-export const updateGames = async(req,res) => {};
+    const newGame = new Games({
+        title,
+        description,
+        price,
+        category,
+        publication_date,
+        platform,
+    });
+    const SaveGame = await newGame.save();
+    res.json(SaveGame);
+};
 
-export const deleteGames = async(req,res) => {};
+export const getGame = async(req,res) => {
+    const game = await Games.findById(req.params.id)
+    if(!game) return res.status(404).json({message: 'Game not found'})
+    res.json(game);
+};
+
+export const getcategory =  async(req,res)=>{
+    const category = req.query.category;
+    const game = await Games.findOne({category:category})
+    if(!game) return res.status(404).json({message: "Game not found"})
+    res.json(game);
+}
+
+export const updateGames = async(req,res) => {
+    const game = await Games.findByIdAndUpdate(req.params.id, req.body, {
+        new:true
+    })
+    if(!game) return res.status(404).json({message:'Game not found'})
+    res.json(game);
+};
+
+export const deleteGames = async(req,res) => {
+    const game = await Games.findByIdAndDelete(req.params.id)
+    if(!game) return res.status(404).json({message:" Game not found"})
+    res.json(game)
+};
--- src/models/games.model.js
+++ src/models/games.model.js
@@ -1,14 +1,32 @@
 import mongoose from "mongoose";
 
-const gameShema = new mongoose.Schema({
+const gameSchema = new mongoose.Schema({
     title:{
         type:String,
+        required:true,
+    },
+    price: {
+        type:Number,
         required:true,
     },
     description: {
         type:String,
         required:true,
+    },
+    platform: {
+        type:String,
+        required:true,
+    },
+    publication_date:{
+        type:Date,
+        default:Date.now
+    },
+    category:{
+        type:String,
+        required:true
     }
 }, {
     timestamps:true
-})
+});
+
+export default mongoose.model("Game", gameSchema);
