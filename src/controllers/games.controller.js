52734884331c054fe0a1fbc387a9259f533873e3
Programador10 <programador10@tecnocomfenalco.edu.co>
2023-11-24 15:06:19 -0500

    modificaciones

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -6,7 +6,7 @@ export const getGames = async(req,res) => {
 };
 
 export const createGames = async(req,res) => {
-    const {title, description,image,commentary, category} = req.body;
+    const {title, description,image, category} = req.body;
     const newGame = new Games({
         title,
         description,
